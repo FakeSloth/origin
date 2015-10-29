afed3739d5d8c055fe964cd7edabf0dc3b750a2f
Phil <birkal@outlook.com>
2015-10-28 23:25:09 -0700

    Add initial deckbuilder implementation

--- deckbuilder/src/app.js
+++ deckbuilder/src/app.js
@@ -1,19 +1,71 @@
-import Aliens from '../../cards/monsters/alien';
-import Angels from '../../cards/monsters/angel';
-import Beasts from '../../cards/monsters/beast';
-import Demons from '../../cards/monsters/demon';
-import Dragons from '../../cards/monsters/dragon';
-import Fusions from '../../cards/monsters/fusion';
+import Vue from 'vue';
 import Humans from '../../cards/monsters/human';
-import Mechs from '../../cards/monsters/mech';
-import Spells from '../../cards/spells/spells';
-import Traps from '../../cards/traps/traps';
 
-$('.container').append($('<code>').text(JSON.stringify(Aliens, null, 2)));
-
-/*
-$('.container').append($('<ul>'));
-Aliens.forEach(function (alien) {
-    $('.container').append($('<li>').text(alien.name));
+var Card = Vue.extend({ 
+	props: ['card'], 
+	template: ` 
+		<div> 
+			<h1>{{card.name}} x{{card.copy}}</h1> 
+			Cost: {{card.cost}} Life: {{card.life}} Attack: {{card.attack}} 
+			Effect: {{card.effect}} 
+		</div> 
+	` 
 });
-*/
+
+Vue.component('card', Card);
+
+let humans = Humans.map(h => {
+	h.copy = 2;
+	if (h.limited) h.copy = 1;
+	return h;
+});
+
+/**
+ * Adds a card from src to target.
+ *
+ * (1) When card is limited, remove card from src and put it into the target.
+ * (2) When there are two copies of a card (src has 2 copies and target has 0),
+ * remove card from src, set the copy prop equal to 1,
+ * and push it to src AND target.
+ * (3) When there is only one copy of a card (src has 1 and target has 1 copy),
+ * remove card from src and target, set the copy prop equal to 2,
+ * and push it back ONLY to target.
+ *
+ * @param {Array} src
+ * @param {Array} target
+ * @param {Object} card
+ */
+function add(src, target, card) {
+	src.$remove(card);
+	if (card.limited) return target.push(card);
+	if (card.copy === 2) {
+		card.copy = 1;
+		src.push(card);
+	} else {
+		target.$remove(card);
+		card.copy = 2;
+	}
+	target.push(card);
+}
+
+new Vue({
+	el: '#app',
+	data: {
+		cards: humans,
+		deck: []
+	},
+	methods: {
+		addToDeck(card) {
+			if (this.deck.length >= 30) return;
+			add(this.cards, this.deck, card);
+		},
+
+		addToCollection(card) {
+			add(this.deck, this.cards, card);
+		},
+
+		deckTotal() {
+			return this.deck.reduce((acc, cur) => acc + cur.copy, 0);
+		}
+	}
+});
